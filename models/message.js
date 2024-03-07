3efee6fe04b54c046bf9edff1f1e807811f5a581
EmilyErickson <emilyerickson2002@gmail.com>
2024-03-07 16:55:06 -0600

    Export models

--- models/message.js
+++ models/message.js
@@ -1,6 +1,11 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
 const messageSchema = new Schema({
   title: String,
   text: String,
   timestamp: { type: Date, default: Date.now },
   author: { type: Schema.Types.ObjectId, ref: "User" },
 });
+
+module.exports = mongoose.model("Message", messageSchema);
