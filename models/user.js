013f028e73247b3c3d4f9244cd970565b906dfe5
EmilyErickson <emilyerickson2002@gmail.com>
2024-03-07 16:35:47 -0600

    Create user schema

---
+++ models/user.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  first_name: { type: String, required: true, maxLength: 100 },
+  last_name: { type: String, required: true, maxLength: 100 },
+  username: { type: String, required: true, maxLength: 100 },
+  password: { type: String },
+  is_member: { type: Boolean, default: false },
+});
