0bbc63b40313588a28abf52eb228c87a025fe8cf
EmilyErickson <emilyerickson2002@gmail.com>
2024-03-07 15:24:01 -0600

    Add clubhouse route

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
+const dotenv = require("dotenv").config();
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const mongoose = require("mongoose");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
+const clubhouseRouter = require("./routes/clubhouse");
 
 const app = express();
+
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 10 * 1000, // 10 seconds
+  max: 10,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
+
+const mongoDb = process.env.MONGO_DB_URL;
+mongoose.connect(mongoDb);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "mongo connection error"));
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -21,6 +40,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/clubhouse", clubhouseRouter); // Add clubhouse routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
