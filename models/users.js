673390ce2b885e3644cf32eb6f4750fc39de35be
EmilyErickson <emilyerickson2002@gmail.com>
2024-03-11 15:26:16 -0500

    Create user model

--- models/users.js
+++ models/users.js
@@ -9,6 +9,8 @@ const userSchema = new mongoose.Schema({
   isAdmin: { type: Boolean, default: false },
 });
 
+userSchema.index({ email: 1 }, { unique: true });
+
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
