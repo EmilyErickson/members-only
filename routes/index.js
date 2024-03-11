e28079ffc7c44e9674de7545def42a24e2cdf82d
EmilyErickson <emilyerickson2002@gmail.com>
2024-03-11 15:30:58 -0500

    Create route get and post functions

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const controller = require("../controllers/controller");
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
-});
+router.get("/", controller.index);
+
+router.get("/sign-up", controller.signUp_create_get);
+router.post("/sign-up", controller.signUp_create_post);
+
+router.get("/login", controller.login_create_get);
+router.post("/login", controller.login_create_post);
+
+router.get("/membership", controller.member_create_get);
+router.post("/membership", controller.member_create_post);
+
+router.get("/admin", controller.admin_create_get);
+router.post("/admin", controller.admin_create_post);
 
 module.exports = router;
