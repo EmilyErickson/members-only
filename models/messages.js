90d29a088d8df388594b9bfad9df67ca161c3c34
EmilyErickson <emilyerickson2002@gmail.com>
2024-03-12 19:08:51 -0500

    Reorder schema to match message display

--- models/messages.js
+++ models/messages.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 
 const messageSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  timestamp: { type: Date, default: Date.now },
   text: { type: String, required: true },
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  timestamp: { type: Date, default: Date.now },
 });
 
 const Message = mongoose.model("Message", messageSchema);
